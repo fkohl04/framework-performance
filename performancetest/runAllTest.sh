4ff1415e2ffeeeeec6f9593bdf6551519725dcb3
Fabian Kohlmann <fabian.kohlmann@extern.dkbcodefactory.com>
2021-12-29 00:31:06 +0100

    adjust performance test

--- performancetest/runAllTest.sh
+++ performancetest/runAllTest.sh
@@ -1,7 +1,7 @@
 export DURATION=180
 
 userCounts=(100 200 300 400 500 600 700 800 900 1000)
-ports=(8081 8082 8083 8084 8085)
+ports=(8081 8082 8083 8084 8085 8086)
 
 for port in "${ports[@]}"; do
   export SUT_URL="http://localhost:${port}"
