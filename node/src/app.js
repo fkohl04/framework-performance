03177f2700d6b4b3855a8b306ebfe4170c3e49d6
Fabian Kohlmann <fabian.kohlmann@extern.dkbcodefactory.com>
2021-12-29 00:21:36 +0100

    improve node

--- node/src/app.js
+++ node/src/app.js
@@ -11,16 +11,27 @@ register.setDefaultLabels({
 
 prometheus.collectDefaultMetrics({ register })
 
+const httpRequestTimer = new prometheus.Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'Duration of HTTP requests in seconds',
+    labelNames: ['route']
+});
+register.registerMetric(httpRequestTimer);
+
 const server = http.createServer(async (req, res) => {
+    const timer = httpRequestTimer.startTimer()
     const route = url.parse(req.url).pathname
     const thirdPartyUrl = process.env.THIRD_PARTY_URL || "http://localhost"
 
     if (route === '/metrics') {
         res.setHeader('Content-Type', register.contentType)
         res.end(await register.metrics())
+        timer({route: "/metrics"})
     } else {
-        (await fetch(thirdPartyUrl +":8080")).text().then( it =>
-            res.end("Fetched third party result:" + it)
+        (await fetch(thirdPartyUrl +":8080")).text().then( it => {
+                res.end("Fetched third party result:" + it)
+                timer({route: "/"})
+            }
         )
     }
 })
